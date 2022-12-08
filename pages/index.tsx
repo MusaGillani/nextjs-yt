62299d469586611a04ce77f87b2f4c3b5fc2783e
musa <musagillaniwork@gmail.com>
2022-12-09 03:49:58 +0500

    Video 11

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 export default function Home() {
+  const router = useRouter();
+  function handleClick() {
+    router.push("/product");
+    // router.replace("/blog");
+  }
   return (
     <div>
       <h1>Home page</h1>
       <Link href="/blog">Blog</Link>
       <Link href="/product">Product</Link>
+      <button onClick={handleClick}>Navigate</button>
     </div>
   );
 }
