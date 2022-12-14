ddc02c90c69ed359de79fd0436b8a0e01a540f9a
musa <musagillaniwork@gmail.com>
2022-12-14 15:46:42 +0500

    Video 21

--- pages/users.tsx
+++ pages/users.tsx
@@ -23,7 +23,6 @@ export default function UserList({ users }: { users: User[] }) {
 export const getStaticProps: GetStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
-  console.log(data);
   return {
     props: {
       users: data,
