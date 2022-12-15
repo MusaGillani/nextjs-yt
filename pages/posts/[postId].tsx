9d3b00b5598d64938720ba19cb08be5174d147f4
musa <musagillaniwork@gmail.com>
2022-12-15 15:06:28 +0500

    Video 27

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from "next";
+import { useRouter } from "next/router";
 
 type Post = {
   userId: number;
@@ -8,6 +9,11 @@ type Post = {
 };
 
 export default function Post({ post }: { post: Post }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>...is loading</h1>;
+  }
   return (
     <>
       <h2>
@@ -19,24 +25,24 @@ export default function Post({ post }: { post: Post }) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await res.json();
+  // const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  // const data = await res.json();
 
-  const paths = data.map((post: { id: string }) => {
-    return {
-      params: {
-        postId: `${post.id}`,
-      },
-    };
-  });
+  // const paths = data.map((post: { id: string }) => {
+  //   return {
+  //     params: {
+  //       postId: `${post.id}`,
+  //     },
+  //   };
+  // });
   return {
-    // paths: [
-    //   { params: { postId: "1" } },
-    //   { params: { postId: "2" } },
-    //   { params: { postId: "3" } },
-    // ],
-    paths,
-    fallback: false,
+    paths: [
+      { params: { postId: "1" } },
+      { params: { postId: "2" } },
+      { params: { postId: "3" } },
+    ],
+    // paths,
+    fallback: true,
   };
 };
 
@@ -46,6 +52,14 @@ export const getStaticProps: GetStaticProps = async (context) => {
     `https://jsonplaceholder.typicode.com/posts/${params!.postId}`
   );
   const data = await res.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  console.log("Generating page for /posts/", params!.postId);
   return {
     props: {
       post: data,
