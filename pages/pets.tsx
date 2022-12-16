a200873fdfa927deaee3972e182e9d6566fc9b90
musa <musagillaniwork@gmail.com>
2022-12-16 18:14:28 +0500

    Video 58

---
+++ pages/pets.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import img from "../public/1.jpg";
+
+export default function PetsPage() {
+  return (
+    <div>
+      <Image
+        src={img}
+        placeholder="blur"
+        blurDataURL=""
+        alt="pet"
+        width="280"
+        height="420"
+      />
+      {["1", "2", "3", "4", "5"].map((path) => (
+        <div key={path}>
+          <Image src={`/${path}.jpg`} alt="pet" width="280" height="420" />
+        </div>
+      ))}
+    </div>
+  );
+}
