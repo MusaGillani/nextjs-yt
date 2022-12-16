d978e7a378fc49850ece56951f635ba533b8e176
musa <musagillaniwork@gmail.com>
2022-12-16 15:54:56 +0500

    Video 50

--- pages/api/[...params].ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { params } = req.query;
-  console.log(params);
-  res.status(200).json(params);
-}
