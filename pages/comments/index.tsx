546b6163608fd3d509bc8d7b1c1282d264044b0c
musa <musagillaniwork@gmail.com>
2022-12-16 12:55:59 +0500

    Video 43

--- pages/comments/index.tsx
+++ pages/comments/index.tsx
@@ -6,14 +6,33 @@ type Comment = {
 };
 export default function CommentsPage() {
   const [comments, setComments] = useState<Comment[]>([]);
+  const [comment, setComment] = useState<string>("");
 
   const fetchComments = async () => {
     const res = await fetch("/api/comments");
     const data = await res.json();
     setComments(data);
   };
+
+  const submitComment = async () => {
+    const res = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <>
+      <input
+        type="text"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      />
+      <button onClick={submitComment}>Submit Comment</button>
       <button onClick={fetchComments}>Load Comments</button>
       {comments.map((comment) => (
         <div key={comment.id}>
